f174044403f0d5ced2d8d6671ea80ac3ad5d96a5
JeromeHumbert <54804629+JeromeHumbert@users.noreply.github.com>
2019-10-10 17:54:51 +0200

    test save

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
-import re
-s = '<study_first_posted type="Estimate">September 24, 1999</study_first_posted><last_update_submitted>September 16, 2009</last_update_submitted><last_update_submitted_qc>September 16, 2009</last_update_submitted_qc><last_update_posted type="Estimate">September 17, 2009</last_update_posted><condition_browse>'
-s = re.sub('\<last_update_submitted.*?last_update_submitted>', '', s)
-print(s)
-print(s)
+txt = "Jérôme Humbert"
+ind = txt.find(" ")
+txt = txt[0:ind]+" 'L'Assistant' "+txt[ind+1:]
+print(txt)
+
